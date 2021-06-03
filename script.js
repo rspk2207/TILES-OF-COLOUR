706c04b57dff24d6f6c687e43b9718287a5d9874
rspk2207 <83638002+rspk2207@users.noreply.github.com>
2021-06-03 21:08:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let colorr = ['#006400','#00008b','#ff0000','#ffffff','#ffa500','a52a2a'];
+let colorr = ['#006400','#00008b','#ff0000','#ffffff','#ffa500','#a52a2a'];
+for(let i=1;i<10;i++)
+{
 let rand = colorr[Math.floor(Math.random() * colorr.length)];
-document.getElementById('boxf1').style.backgroundColor = rand;
+let g = document.getElementById("boxf" + i);
+g.style.backgroundColor = rand;
+}
+for(let i=1;i<25;i++)
+{
+    rand = colorr[Math.floor(Math.random() * colorr.length)];
+    let s = document.getElementById("box" + i);
+    s.style.backgroundColor = rand;
+}
