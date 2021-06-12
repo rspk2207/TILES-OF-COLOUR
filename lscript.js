9e08fcf67984825512977eea96e71649c3c5d244
rspk2207 <83638002+rspk2207@users.noreply.github.com>
2021-06-13 00:33:53 +0530

    Add files via upload

--- lscript.js
+++ lscript.js
@@ -1,5 +1,33 @@
-let i;
+let i,j;
 let sno = window.localStorage.getItem("count");
+for(i=1;i<=(sno-1);i++)
+{
+    for(j=(i+1);j<=(sno);j++)
+    {
+        if(parseInt(window.localStorage.getItem("scorefinal"+i))<parseInt(window.localStorage.getItem("scorefinal"+j)))
+        {
+            let hscore = window.localStorage.getItem("scorefinal"+j);
+            let hname = window.localStorage.getItem("user"+j);
+            let lscore = window.localStorage.getItem("scorefinal"+i);
+            let lname = window.localStorage.getItem("user"+i);
+            window.localStorage.setItem("scorefinal"+i,hscore);
+            window.localStorage.setItem("scorefinal"+j,lscore);
+            window.localStorage.setItem("user"+i,hname);
+            window.localStorage.setItem("user"+j,lname);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 for(i=1;i<=sno;i++)
 {
 if(window.localStorage.getItem("count"))
